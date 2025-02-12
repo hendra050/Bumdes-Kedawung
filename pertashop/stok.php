528cbd3b6900c9bfa95d2065451fdac27983ed62
hendra050 <kingofdevil157@gmail.com>
2025-02-12 07:24:49 +0700

    perubahan git

--- pertashop/stok.php
+++ pertashop/stok.php
@@ -21,12 +21,65 @@
           <div class="box-header">
             <h3 class="box-title">Total Stok Yang Dimiliki</h3>
             <div class="btn-group pull-right">            
-              <a href="stok_act.php"><button type="button" class="btn btn-success btn-sm">
-                <i class="fa fa-file-excel-o"></i> &nbsp Update Data Stok
-              </button></a>
+              <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#exampleModal">
+                <i class="fa fa-plus"></i> &nbsp Tambah Pengeluaran
+              </button>
             </div>
           </div>
           <div class="box-body">
+
+            <!-- Modal -->
+            <form action="pengeluaran_act.php" method="post">
+              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h4 class="modal-title" id="exampleModalLabel">Tambah Transaksi</h4>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+
+                      <div class="form-group">
+                        <label>Tanggal</label>
+                        <input type="text" name="tanggal" required="required" class="form-control datepicker2">
+                      </div>
+
+                      <div class="form-group">
+                        <label>Kategori</label>
+                        <select name="kategori" class="form-control" required="required">
+                          <option value="">- Pilih -</option>
+                          <?php 
+                          $kategori = mysqli_query($koneksi,"SELECT * FROM kategori_pertashop ORDER BY kategori ASC");
+                          while($k = mysqli_fetch_array($kategori)){
+                            ?>
+                            <option value="<?php echo $k['kategori_id']; ?>"><?php echo $k['kategori']; ?></option>
+                            <?php 
+                          }
+                          ?>
+                        </select>
+                      </div>
+                      
+                      <div class="form-group">
+                        <label>Keterangan</label>
+                        <textarea name="keterangan" class="form-control" rows="3"></textarea>
+                      </div>
+
+                      <div class="form-group">
+                        <label>Nominal</label>
+                        <input type="number" name="nominal" required="required" class="form-control" placeholder="Masukkan Nominal ..">
+                      </div>
+
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                      <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
 
             <div class="table-responsive">
               <table class="table table-bordered table-striped" id="table-datatable">
